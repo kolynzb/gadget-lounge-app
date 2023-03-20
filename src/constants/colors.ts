e9702601343c1814377492b959d4165af402faef
kolynzb <collinsbenda360@gmail.com>
2023-03-20 20:22:23 +0300

    feat: add color constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,18 @@
+const colors = {
+  primary: '#202c41',
+  border: '#c6c6c6',
+  white: '#FFFFFF',
+  background: '#eef0f3',
+  gray: {
+    1: '#ECECEC',
+    2: '#F5F5F5',
+    3: '#C7C6C5',
+  },
+  black: {
+    1: '#101010',
+  },
+  red: '#CF0B21',
+  error: '#b55464',
+};
+
+export default colors;
