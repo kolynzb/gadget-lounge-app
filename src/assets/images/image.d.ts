cf2945055130c8691d701619ffb639360d63fff8
kolynzb <collinsbenda360@gmail.com>
2023-03-20 20:23:05 +0300

    fix: add types for image import

---
+++ src/assets/images/image.d.ts
@@ -0,0 +1,7 @@
+declare module "*.png" {
+  import { ImageSourcePropType } from "react-native";
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+// https://exchangetuts.com/react-native-typescript-images-static-assets-1640154123611521
