88028ad72bd80a0d92a3c6b3745d041e473674f7
kolynzb <collinsbenda360@gmail.com>
2023-03-23 01:08:11 +0300

    chore: create special offer dummy data

---
+++ src/data/specialOffers.ts
@@ -0,0 +1,11 @@
+const specialOffers = [
+  {
+    percentage: 20,
+    title: "Today's Special!",
+    message: 'Get discount for every order, only valide for today',
+    image:
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+  },
+];
+
+export default specialOffers;
