1c7c74f6d3c5d85577cd74002456f2908404a384
kolynzb <collinsbenda360@gmail.com>
2023-03-23 00:19:16 +0300

    feat add spacer

--- src/screens/Homescreen.tsx
+++ src/screens/Homescreen.tsx
@@ -1,7 +1,7 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import WelcomeHeaderSection from '../components/WelcomeHeaderSection';
-import {ScreenView} from '../components/layout';
+import {ScreenView, Spacer} from '../components/layout';
 import SearchBar from '../components/SearchBar';
 
 type Props = {};
@@ -13,6 +13,7 @@ const Homescreen = (props: Props) => {
         name="Sarah Schaller"
         avatar="https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg"
       />
+      <Spacer size={15} />
       <SearchBar />
     </ScreenView>
   );
