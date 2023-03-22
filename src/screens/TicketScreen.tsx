47e23fc51ebf761ea670e227adbb60f23b2ef182
kolynzb <collinsbenda360@gmail.com>
2023-03-22 23:07:27 +0300

    feat: create dummy ticket screen

---
+++ src/screens/TicketScreen.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const TicketScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>TicketsScreen</Text>
+    </View>
+  );
+};
+
+export default TicketScreen;
+
+const styles = StyleSheet.create({});
