a237baec86652974cb7955a1d1c14db1ed81bf21
kolynzb <collinsbenda360@gmail.com>
2023-03-22 23:06:16 +0300

    feat: create weelcome screen

---
+++ src/screens/onboarding/WelcomeScreen.tsx
@@ -0,0 +1,16 @@
+import {Text, View, StyleSheet} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const WelcomeScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>WelcomeScreen</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({});
