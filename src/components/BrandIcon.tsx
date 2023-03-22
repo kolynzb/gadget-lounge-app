7c63d876f76cbad2687e6cc6ce54b71bee7b9f00
kolynzb <collinsbenda360@gmail.com>
2023-03-23 02:54:39 +0300

    feat: create brnad icon

--- src/components/BrandIcon.tsx
+++ src/components/BrandIcon.tsx
@@ -1,12 +1,22 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Image, ImageURISource} from 'react-native';
 import React from 'react';
 import {Text, View} from './layout';
+import colors from '../constants/colors';
 
-type Props = {};
+type Props = {
+  name: string;
+  image: ImageURISource['uri'];
+};
 
 const BrandIcon = (props: Props) => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: props.image,
+        }}
+        style={styles.logo}
+      />
       <Text>BrandIcon</Text>
     </View>
   );
@@ -14,4 +24,19 @@ const BrandIcon = (props: Props) => {
 
 export default BrandIcon;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  logo: {
+    backgroundColor: colors.gray[4],
+    borderRadius: 50,
+    width: 50,
+    height: 50,
+  },
+  brand: {
+    fontWeight: '400',
+    fontSize: 14,
+  },
+});
