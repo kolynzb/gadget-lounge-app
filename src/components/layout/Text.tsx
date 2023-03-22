fad6946e8b10df604233be0e20b6897d4baa22be
kolynzb <collinsbenda360@gmail.com>
2023-03-22 23:17:39 +0300

    feat: create custom text component

---
+++ src/components/layout/Text.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Text as ReactText,
+  StyleSheet,
+  StyleProp,
+  TextStyle,
+} from 'react-native';
+
+type TextProps = {
+  children: React.ReactNode;
+  style?: StyleProp<TextStyle>;
+};
+
+const Text = ({style, children}: TextProps) => {
+  return <ReactText style={[styles.font, style]}>{children}</ReactText>;
+};
+export default Text;
+
+const styles = StyleSheet.create({
+  font: {
+    fontFamily: 'Nunito-Regular',
+  },
+});
