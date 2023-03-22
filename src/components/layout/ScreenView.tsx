4495893029cf7d8481d3d49b06da75ed41712891
kolynzb <collinsbenda360@gmail.com>
2023-03-22 23:40:26 +0300

    feat: create custome screen view

---
+++ src/components/layout/ScreenView.tsx
@@ -0,0 +1,26 @@
+import {
+  StyleSheet,
+  ViewStyle,
+  StyleProp,
+  SafeAreaView,
+} from 'react-native';
+import React from 'react';
+
+type Props = {
+  children: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+};
+
+const ScreenView = (props: Props) => {
+  return (
+    <SafeAreaView style={[styles.container, props.style]}>
+      {props.children}
+    </SafeAreaView>
+  );
+};
+
+export default ScreenView;
+
+const styles = StyleSheet.create({
+  container: {},
+});
