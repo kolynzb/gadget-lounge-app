67cba03aafdc7c9408c7525f7a72d4afa67a8abd
kolynzb <collinsbenda360@gmail.com>
2023-03-22 23:46:03 +0300

    feat: create search bar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+import React from 'react';
+import {Text, View} from './layout';
+
+type Props = {};
+
+const SearchBar = (props: Props) => {
+  return (
+    <View style={styles.container}>
+      <Text>SearchBar</Text>
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  container: {},
+});
