8e6e217abd891637a631b7d18f8e606984ca2d43
kolynzb <collinsbenda360@gmail.com>
2023-03-23 01:09:06 +0300

    feat: create brand icon component

---
+++ src/components/BrandIcon.tsx
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+import React from 'react';
+import {Text, View} from './layout';
+
+type Props = {};
+
+const BrandIcon = (props: Props) => {
+  return (
+    <View>
+      <Text>BrandIcon</Text>
+    </View>
+  );
+};
+
+export default BrandIcon;
+
+const styles = StyleSheet.create({});
