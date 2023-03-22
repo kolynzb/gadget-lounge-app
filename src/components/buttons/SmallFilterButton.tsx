1fb75ff70f9c08ce83b89742a848c3bb8754d2df
kolynzb <collinsbenda360@gmail.com>
2023-03-23 02:57:22 +0300

    feat: add touchable opacity

--- src/components/buttons/SmallFilterButton.tsx
+++ src/components/buttons/SmallFilterButton.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, Pressable} from 'react-native';
+import {StyleSheet, Pressable, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {Text} from '../layout';
 import colors from '../../constants/colors';
@@ -10,15 +10,17 @@ type Props = {
 
 const SmallFilterButton = ({text, active = false}: Props) => {
   return (
-    <Pressable
-      style={[
-        styles.button,
-        {backgroundColor: active ? colors.black[1] : 'white'},
-      ]}>
-      <Text style={[styles.text, {color: active ? 'white' : 'black'}]}>
-        {text}
-      </Text>
-    </Pressable>
+    <TouchableOpacity>
+      <Pressable
+        style={[
+          styles.button,
+          {backgroundColor: active ? colors.black[1] : 'white'},
+        ]}>
+        <Text style={[styles.text, {color: active ? 'white' : 'black'}]}>
+          {text}
+        </Text>
+      </Pressable>
+    </TouchableOpacity>
   );
 };
 
