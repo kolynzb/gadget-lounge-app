12ca6644611d43131cb37ed6325a1502daa0f899
kolynzb <collinsbenda360@gmail.com>
2023-03-23 04:36:30 +0300

    chore: add more brands

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -1,8 +1,48 @@
 const brands = [
   {
     id: 0,
-    logo: '',
+    logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
     name: 'samsung',
+  },
+  {
+    id: 0,
+    logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+    name: 'Apple',
+  },
+  {
+    id: 0,
+    logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+    name: 'Infinix',
+  },
+  {
+    id: 0,
+    logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+    name: 'samsung',
+  },
+  {
+    id: 0,
+    logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+    name: 'Tecno',
+  },
+  {
+    id: 0,
+    logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+    name: 'Google',
+  },
+  {
+    id: 0,
+    logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+    name: 'Lenovo',
+  },
+  {
+    id: 0,
+    logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+    name: 'Huawei',
+  },
+  {
+    id: 0,
+    logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+    name: 'Nokia',
   },
 ];
 
