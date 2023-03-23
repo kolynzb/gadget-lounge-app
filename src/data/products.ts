66a085e4014a1beb405b4a06c94a6fdaf0238c30
kolynzb <collinsbenda360@gmail.com>
2023-03-23 04:30:32 +0300

    feat: add more products

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,10 +1,39 @@
 const products = [
   {
-    id:0,
-    name: '',
-    brand: '',
-    price: 0,
+    id: 0,
+    name: 'Iphone 13 pro Max SE limited Edition',
+    brand: 'äpple',
+    price: 4000000,
     rating: 4.3,
+    image:
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+  },
+  {
+    id: 0,
+    name: 'Iphone 13 pro Max SE limited Edition',
+    brand: 'äpple',
+    price: 4000000,
+    rating: 4.3,
+    image:
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+  },
+  {
+    id: 0,
+    name: 'Iphone 13 pro Max SE limited Edition',
+    brand: 'äpple',
+    price: 4000000,
+    rating: 4.3,
+    image:
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+  },
+  {
+    id: 0,
+    name: 'Iphone 13 pro Max SE limited Edition',
+    brand: 'äpple',
+    price: 4000000,
+    rating: 4.3,
+    image:
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
   },
 ];
 
