bbb0bb2a7f4a131ab8ea7f351657e84dbd76210b
kolynzb <collinsbenda360@gmail.com>
2023-04-19 16:30:17 +0300

    chore: add description and reviews

--- src/data/products.ts
+++ src/data/products.ts
@@ -1,10 +1,15 @@
-const products = [
+import {IProduct} from '../interfaces/product.interface';
+
+const products: IProduct[] = [
   {
     id: 0,
     name: 'Iphone 13 pro Max SE limited Edition',
     brand: 'äpple',
     price: 4000000,
     rating: 4.3,
+    reviews: 3600,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     image:
       'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
   },
@@ -12,8 +17,11 @@ const products = [
     id: 1,
     name: 'Iphone 13 pro Max SE limited Edition',
     brand: 'äpple',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     price: 4000000,
     rating: 4.3,
+    reviews: 3600,
     image:
       'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
   },
@@ -21,8 +29,11 @@ const products = [
     id: 2,
     name: 'Iphone 13 pro Max SE limited Edition',
     brand: 'äpple',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     price: 4000000,
     rating: 4.3,
+    reviews: 3600,
     image:
       'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
   },
@@ -30,8 +41,11 @@ const products = [
     id: 3,
     name: 'Iphone 13 pro Max SE limited Edition',
     brand: 'äpple',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     price: 4000000,
     rating: 4.3,
+    reviews: 3600,
     image:
       'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
   },
