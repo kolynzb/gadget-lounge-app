fba792ff0d3de2022aa88ca775fc5f97205de024
kolynzb <collinsbenda360@gmail.com>
2023-04-20 02:34:54 +0300

    feat: add actual product images

--- src/data/products.ts
+++ src/data/products.ts
@@ -9,68 +9,105 @@ const products: IProduct[] = [
     rating: 4.3,
     reviews: 3600,
     images: [
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-      "'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg'",
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/rahul-chakraborty-xsGxhtAsfSA-unsplash-removebg-preview_zfbf3e.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/neil-soni-6wdRuK7bVTE-unsplash-removebg-preview_zvhpaf.png',
     ],
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     image:
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
   },
   {
     id: 1,
     name: 'Iphone 13 pro Max SE limited Edition',
     brand: 'äpple',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     price: 4000000,
-    images: [
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-    ],
-
     rating: 4.3,
     reviews: 3600,
+    images: [
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/rahul-chakraborty-xsGxhtAsfSA-unsplash-removebg-preview_zfbf3e.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/neil-soni-6wdRuK7bVTE-unsplash-removebg-preview_zvhpaf.png',
+    ],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     image:
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
   },
   {
     id: 2,
     name: 'Iphone 13 pro Max SE limited Edition',
     brand: 'äpple',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     price: 4000000,
-    images: [
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-    ],
-
     rating: 4.3,
     reviews: 3600,
+    images: [
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/rahul-chakraborty-xsGxhtAsfSA-unsplash-removebg-preview_zfbf3e.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/neil-soni-6wdRuK7bVTE-unsplash-removebg-preview_zvhpaf.png',
+    ],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     image:
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
   },
   {
     id: 3,
     name: 'Iphone 13 pro Max SE limited Edition',
     brand: 'äpple',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     price: 4000000,
-    images: [
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
-    ],
-
     rating: 4.3,
     reviews: 3600,
+    images: [
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/rahul-chakraborty-xsGxhtAsfSA-unsplash-removebg-preview_zfbf3e.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/neil-soni-6wdRuK7bVTE-unsplash-removebg-preview_zvhpaf.png',
+    ],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
     image:
-      'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+  },
+  {
+    id: 4,
+    name: 'Iphone 13 pro Max SE limited Edition',
+    brand: 'äpple',
+    price: 4000000,
+    rating: 4.3,
+    reviews: 3600,
+    images: [
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/rahul-chakraborty-xsGxhtAsfSA-unsplash-removebg-preview_zfbf3e.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/neil-soni-6wdRuK7bVTE-unsplash-removebg-preview_zvhpaf.png',
+    ],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
+    image:
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/rahul-chakraborty-xsGxhtAsfSA-unsplash-removebg-preview_zfbf3e.png',
+  },
+  {
+    id: 5,
+    name: 'Iphone 13 pro Max SE limited Edition',
+    brand: 'äpple',
+    price: 4000000,
+    rating: 4.3,
+    reviews: 3600,
+    images: [
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/rahul-chakraborty-xsGxhtAsfSA-unsplash-removebg-preview_zfbf3e.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/neil-soni-6wdRuK7bVTE-unsplash-removebg-preview_zvhpaf.png',
+    ],
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, obcaecati pariatur? Quia, sint aliquam ullam eligendi magnam odit sapiente! Porro incidunt delectus pariatur impedit recusandae laboriosam sequi nihil maxime optio! Sequi praesentium dolores, quasi sed quas quos voluptatem libero culpa!',
+    image:
+      'https://res.cloudinary.com/kolynz-b/image/upload/v1681945504/rahul-chakraborty-xsGxhtAsfSA-unsplash-removebg-preview_zfbf3e.png',
   },
 ];
 
