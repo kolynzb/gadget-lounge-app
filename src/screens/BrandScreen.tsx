09061d3a5f48346ea16f191081564719c4199abe
kolynzb <collinsbenda360@gmail.com>
2023-04-20 07:50:32 +0300

    feat: add re-animated items to page

--- src/screens/BrandScreen.tsx
+++ src/screens/BrandScreen.tsx
@@ -1,10 +1,4 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Dimensions,
-  Alert,
-} from 'react-native';
+import {StyleSheet, Text, View, Dimensions, Alert} from 'react-native';
 import React from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
@@ -17,15 +11,16 @@ import Animated, {
   useAnimatedStyle,
 } from 'react-native-reanimated';
 import brands from '../data/brands';
+import {IBrand} from '../interfaces/brand.interface';
 
-const { width, height } = Dimensions.get("window");
+const {width, height} = Dimensions.get('window');
 const MIN_HEIGHT = 128;
 export const MAX_HEIGHT = height / 2;
 const itemStyles = StyleSheet.create({
   container: {
     width,
     height: MIN_HEIGHT,
-    justifyContent: "flex-end",
+    justifyContent: 'flex-end',
   },
   picture: {
     ...StyleSheet.absoluteFillObject,
@@ -33,97 +28,90 @@ const itemStyles = StyleSheet.create({
     height: undefined,
   },
   title: {
-    color: "white",
-    textAlign: "center",
+    color: 'white',
+    textAlign: 'center',
     fontSize: 32,
-    fontWeight: "500",
+    fontWeight: '500',
   },
   titleContainer: {
     maxHeight: MAX_HEIGHT * 0.61,
-    justifyContent: "center",
+    justifyContent: 'center',
     flex: 1,
   },
   mainTitle: {
     ...StyleSheet.absoluteFillObject,
-    justifyContent: "center",
+    justifyContent: 'center',
     padding: 32,
-    transform: [{ translateY: 64 }],
+    transform: [{translateY: 64}],
   },
   subtitle: {
-    color: "white",
-    textAlign: "center",
+    color: 'white',
+    textAlign: 'center',
     fontSize: 16,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
 });
 
-interface Item {
-    title: string;
-    subtitle: string;
-    picture: number;
-    top: number;
-  }
-  
-  interface ItemProps {
-    index: number;
-    y: Animated.SharedValue<number>;
-    item: Item;
-  }
+interface BrandItemProps {
+  index: number;
+  y: Animated.SharedValue<number>;
+  item: IBrand;
+}
 
-const Item = ({
-    y,
-    index,
-    item: { title, subtitle, picture, top },
-  }: ItemProps) => {
-    const style = useAnimatedStyle(() => {
-      return {
-        height: interpolate(
-          y.value,
-          [(index - 1) * MAX_HEIGHT, index * MAX_HEIGHT],
-          [MIN_HEIGHT, MAX_HEIGHT],
-          Extrapolate.CLAMP
-        ),
-      };
-    });
-    const titleStyle = useAnimatedStyle(() => {
-      const opacity = interpolate(
+const BrandItem: React.FC<BrandItemProps> = ({
+  y,
+  index,
+  item: {name, slogan, image, top},
+}) => {
+  const style = useAnimatedStyle(() => {
+    return {
+      height: interpolate(
         y.value,
         [(index - 1) * MAX_HEIGHT, index * MAX_HEIGHT],
-        [0, 1],
-        Extrapolate.CLAMP
-      );
-      return {
-        opacity,
-      };
-    });
-    const pictureStyle = useAnimatedStyle(() => ({
-      height: MAX_HEIGHT,
-      top: interpolate(
-        y.value,
-        [(index - 1) * MAX_HEIGHT, index * MAX_HEIGHT],
-        [-top, 0]
+        [MIN_HEIGHT, MAX_HEIGHT],
+        Extrapolate.CLAMP,
       ),
-    }));
-    return (
-      <TouchableWithoutFeedback onPress={() => Alert.alert("Pressed!")}>
-        <Animated.View style={[itemStyles.container, style]}>
-          <Animated.Image
-            source={picture}
-            style={[itemStyles.picture, pictureStyle]}
-          />
-          <View style={itemStyles.titleContainer}>
-            <Text style={itemStyles.subtitle}>{subtitle.toUpperCase()}</Text>
-            <View style={itemStyles.mainTitle}>
-              <Animated.View style={titleStyle}>
-                <Text style={itemStyles.title}>{title.toUpperCase()}</Text>
-              </Animated.View>
-            </View>
-          </View>
-        </Animated.View>
-      </TouchableWithoutFeedback>
+    };
+  });
+  const titleStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      y.value,
+      [(index - 1) * MAX_HEIGHT, index * MAX_HEIGHT],
+      [0, 1],
+      Extrapolate.CLAMP,
     );
-  };
-  
+    return {
+      opacity,
+    };
+  });
+  const pictureStyle = useAnimatedStyle(() => ({
+    height: MAX_HEIGHT,
+    top: interpolate(
+      y.value,
+      [(index - 1) * MAX_HEIGHT, index * MAX_HEIGHT],
+      [-top, 0],
+    ),
+  }));
+  return (
+    <TouchableWithoutFeedback onPress={() => Alert.alert('Pressed!')}>
+      <Animated.View style={[itemStyles.container, style]}>
+        <Animated.Image
+          source={{uri: image}}
+          style={[itemStyles.picture, pictureStyle]}
+        />
+        <View style={itemStyles.titleContainer}>
+          <Text style={itemStyles.subtitle}>{slogan.toUpperCase()}</Text>
+          <View style={itemStyles.mainTitle}>
+            <Animated.View style={titleStyle}>
+              <Text style={itemStyles.title}>{name.toUpperCase()}</Text>
+            </Animated.View>
+          </View>
+        </View>
+      </Animated.View>
+    </TouchableWithoutFeedback>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     height: (brands.length + 1) * MAX_HEIGHT,
@@ -147,8 +135,8 @@ const BrandScreen = () => {
         scrollEventThrottle={16}
         snapToInterval={MAX_HEIGHT}>
         <Animated.View style={styles.container}>
-          {brands.map((item: any, index: any) => (
-            <Item item={item} key={index} y={y} index={index} />
+          {brands.map((item: IBrand, index: number) => (
+            <BrandItem item={item} key={index} y={y} index={index} />
           ))}
         </Animated.View>
       </Animated.ScrollView>
