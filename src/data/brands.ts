2fcb25179de524685126d33658de702c08a56b25
kolynzb <collinsbenda360@gmail.com>
2023-04-20 07:50:09 +0300

    feat: update brand object and assign type

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -1,48 +1,80 @@
-const brands = [
+import {IBrand} from '../interfaces/brand.interface';
+
+const brands: IBrand[] = [
   {
     id: 0,
     logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
     name: 'samsung',
+    slogan: 'Lorem ipsum dolor sit amet.',
+    image: '',
+    top: 0,
   },
   {
     id: 0,
     logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
     name: 'Apple',
+    slogan: 'Lorem ipsum dolor sit amet.',
+
+    top: 0,
+    image: '',
   },
   {
     id: 1,
     logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
     name: 'Infinix',
+    slogan: 'Lorem ipsum dolor sit amet.',
+    image: '',
+
+    top: 0,
   },
   {
     id: 2,
     logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
+    slogan: 'Lorem ipsum dolor sit amet.',
+    image: '',
+
     name: 'samsung',
+    top: 0,
   },
   {
     id: 3,
     logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
     name: 'Tecno',
+    slogan: 'Lorem ipsum dolor sit amet.',
+    image: '',
+    top: 0,
   },
   {
     id: 5,
     logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
     name: 'Google',
+    slogan: 'Lorem ipsum dolor sit amet.',
+    image: '',
+    top: 0,
   },
   {
     id: 6,
     logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
     name: 'Lenovo',
+    slogan: 'Lorem ipsum dolor sit amet.',
+    image: '',
+    top: 0,
   },
   {
     id: 7,
     logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
     name: 'Huawei',
+    slogan: 'Lorem ipsum dolor sit amet.',
+    image: '',
+    top: 0,
   },
   {
     id: 8,
     logo: 'https://res.cloudinary.com/kolynz-b/image/upload/v1638636909/ko.lynz_b_218871186_831566384142117_7643572219233961744_n_wcsj3e.jpg',
     name: 'Nokia',
+    slogan: 'Lorem ipsum dolor sit amet.',
+    image: '',
+    top: 0,
   },
 ];
 
