4053d16d1027e92bcfc8af67761a3715cfe9cee2
kolynzb <collinsbenda360@gmail.com>
2023-04-22 15:38:11 +0300

    chore: add images

--- src/data/onboarding.ts
+++ src/data/onboarding.ts
@@ -5,18 +5,24 @@ export const ONBOARDINGPAGES: IOnboardingPage[] = [
     title: 'Samurai',
     description:
       'A durable deck featured with a menacing face of a samurai at the center of the underside accompanied with a large red sun motif.',
-    source: require('./assets/skates/01.png'),
+    source: {
+      uri: 'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+    },
   },
   {
     title: 'Reject',
     description:
       "You don't have time to consider wheter the graphic on your CSS board would be considered modernist.",
-    source: require('./assets/skates/02.png'),
+    source: {
+      uri: 'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+    },
   },
   {
     title: 'Great Wave',
     description:
       'The top of the deck has the same matching graphic in black outline and embodies an overall mellow concave.',
-    source: require('./assets/skates/03.png'),
+    source: {
+      uri: 'https://res.cloudinary.com/kolynz-b/image/upload/v1681945494/oscar-nord-8l9VxXI28tY-unsplash-removebg-preview_cnedqm.png',
+    },
   },
 ];
