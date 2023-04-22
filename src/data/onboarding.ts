bbe45aa5d99fdc4d59d9c53dfcbaec251d1a89b5
kolynzb <collinsbenda360@gmail.com>
2023-04-22 15:31:47 +0300

    dummy data

---
+++ src/data/onboarding.ts
@@ -0,0 +1,22 @@
+import {IOnboardingPage} from '../interfaces/onboarding.interface';
+
+export const ONBOARDINGPAGES: IOnboardingPage[] = [
+  {
+    title: 'Samurai',
+    description:
+      'A durable deck featured with a menacing face of a samurai at the center of the underside accompanied with a large red sun motif.',
+    source: require('./assets/skates/01.png'),
+  },
+  {
+    title: 'Reject',
+    description:
+      "You don't have time to consider wheter the graphic on your CSS board would be considered modernist.",
+    source: require('./assets/skates/02.png'),
+  },
+  {
+    title: 'Great Wave',
+    description:
+      'The top of the deck has the same matching graphic in black outline and embodies an overall mellow concave.',
+    source: require('./assets/skates/03.png'),
+  },
+];
